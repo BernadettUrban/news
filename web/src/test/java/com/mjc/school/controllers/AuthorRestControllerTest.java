e3ba5d3a0bec9b5d1192eba53c6f0b15f068d703
Bernadett Urban <bernadett.urban@outlook.com>
2024-08-06 16:31:11 +0200

    create profiles for test and dev

--- web/src/test/java/com/mjc/school/controllers/AuthorRestControllerTest.java
+++ web/src/test/java/com/mjc/school/controllers/AuthorRestControllerTest.java
@@ -9,11 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class AuthorRestControllerTest {
 
     @LocalServerPort
--- web/src/test/java/com/mjc/school/controllers/CommentRestControllerTest.java
+++ web/src/test/java/com/mjc/school/controllers/CommentRestControllerTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class CommentRestControllerTest {
 
     @LocalServerPort
